25b6d8bc3cb5886174b0f251572fcfc85240bd82
Guilherme Berger <guilherme.berger@gmail.com>
2014-11-05 02:56:38 -0500

    server writes to tempfile

--- HW5/HW5server.c
+++ HW5/HW5server.c
@@ -51,6 +51,9 @@
 #include "Socket.h"
 #include "HW5.h" /* definitions shared by client and server */
 
+#define FILENAME_SIZE 64
+#define INFOSTR_SIZE 256
+
 
 void shell_service(Socket connect_socket);
 
@@ -111,12 +114,24 @@ int main(int argc, char* argv[]) {
 }
 
 void shell_service(Socket connect_socket) {
-  int i, c, rc;
-  int count = 0;
+  int i, c, rc, len;
   bool forever = true;
-  char line_data[MAX_LINE]; 
+  char input_data[MAX_LINE];
+  char info_string[INFOSTR_SIZE];
+
+  // setupd the temp file
+  pid_t my_pid = getpid();
+  char filename[FILENAME_SIZE] = {0};
+  sprintf(filename, "tmp%d", my_pid);
+  FILE *tmpfile;
+
+  printf("New connection: %d\n", (int)connect_socket);
+  printf("Filename: %s\n", filename);
 
   while (forever) { // actually, until EOF on connect socket
+    // open the tempfile. Also erases all contents
+    tmpfile = fopen(filename, "w");
+
     /* get a null-terminated string from the client on the data
      * transfer socket up to the maximim input line size. Continue 
      * getting strings from the client until EOF on the socket.
@@ -125,26 +140,30 @@ void shell_service(Socket connect_socket) {
       c = Socket_getc(connect_socket);
       if (c == EOF) {
         // assume socket EOF ends service for this client
-        printf("Socket_getc EOF or error\n"); 
-        return; 
+        printf("End connection: %d\n", (int)connect_socket); 
+        return;
       } else {
+        input_data[i] = c;
         if (c == '\0') {
-          line_data[i] = c;
           break;
         }
-        line_data[i] = toupper(c);
       }
     }
 
     // be sure the string is terminated if max size reached
     if (i == MAX_LINE) {
-      line_data[i-1] = '\0';
+      input_data[i-1] = '\0';
     }
 
-    // send it back to the client on the data transfer socket
-    count = strlen(line_data) + 1; // include '\0' in count
-    for (i = 0; i < count; i++) {
-      c = line_data[i];
+    // write to tempfile
+    fprintf(tmpfile, "%s", input_data);
+    fclose(tmpfile);
+
+    // set the file back to start for reading
+    tmpfile = fopen(filename, "r");
+
+    // read from the file and send it back to the client
+    while ((c = fgetc(tmpfile)) != EOF) {
       rc = Socket_putc(c, connect_socket);
       if (rc == EOF) {
         // assume socket EOF ends service for this client
@@ -152,6 +171,25 @@ void shell_service(Socket connect_socket) {
         return;
       }
     }
+
+    sprintf(info_string, "Informational string. Blabla. Input had size of %d.\n", strlen(input_data));
+
+    // +1 for including '\0'
+    len = strlen(info_string) + 1;
+    for(i = 0; i < len; i++) {
+      rc = Socket_putc(info_string[i], connect_socket);
+      if (rc == EOF) {
+        // assume socket EOF ends service for this client
+        printf("Socket_putc EOF or error\n");
+        return;
+      }
+    }
+
+    // close the tempfile
+    fclose(tmpfile);
   } 
   // end while loop of the service process
+
+  // remove the temporary file
+  remove(filename);
 }
